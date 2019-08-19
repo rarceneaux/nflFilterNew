ce60e431cf9eeea9ab7ac921578ecf9caad01e01
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-08-18 20:24:33 -0500

    jumbo and btns

---
+++ javascripts/helpers/util.js
@@ -0,0 +1,13 @@
+const printToDom = (stringToPrint,divId) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML += stringToPrint;
+};
+
+
+
+
+
+
+
+
+export {printToDom};
